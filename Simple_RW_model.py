db4ef5683f95311a36978fb0f4be7eca2bfd7a13
MaudBeeckmans <73498415+MaudBeeckmans@users.noreply.github.com>
2021-09-29 15:08:03 +0200

    Update Simple_RW_model.py
    * updated version of the simple RW-model

--- Simple_RW_model.py
+++ Simple_RW_model.py
@@ -12,23 +12,29 @@ Created on Tue Sep 28 17:47:49 2021
 import pandas
 import numpy as np
 
+
+def softmax(current_values = np.array([0, 0]), temperature = 1): 
+    probabilities = np.exp(current_values*temperature) / np.sum((np.exp(current_values[0]*temperature)+np.exp(current_values[1]*temperature)))
+    # is the softmax function
+    return probabilities
+
 #function to choose option L with probability 'probabilityL'
     #returns 0 when optionL is not chosen, 1 when optionL is chosen 
-def choose(prob = 0.5): 
-    choice = (np.random.random() <= prob)*1 
+def choose_option(prob = np.array([0.5, 0.5])): 
+    choice = (np.random.random() <= prob[1])*1 
         #whether you choose the option that has probability prob or not 
         # 1 if you choose this option, 0 if you don't 
     return choice
+
+def get_reward(prob = 0.5): 
+    rew_present = (np.random.random() <= prob)*1
+    return rew_present
 
 def rescorla_wagner(previous_value = 0, obtained_rew = 0, learning_rate = 0.1): 
     PE = obtained_rew - previous_value
     value = previous_value + PE*learning_rate
     return PE, value 
     
-def softmax(valueL = 0, valueR = 0, temperature = 1): 
-    probability_R = np.exp(valueR*temperature) / (np.exp(valueL*temperature)+np.exp(valueR*temperature))
-    # is the softmax function
-    return probability_R
 
 alpha = 0.1
 gamma = 1
@@ -38,21 +44,21 @@ values = np.array([0.0, 0.0])
 rew_probabilities = np.array([0.2, 0.8])
 
 for trial in range(n_trials): 
-    probability_R = softmax(valueL = values[0], valueR = values[1], temperature = gamma)
-    #print("The chance of choosing option R is {}".format(probability_R))
-    choose_R = choose(prob = probability_R)
-    #print("Did we choose option R? {}".format(choose_R))
-        # choose_R contains whether option R was chosen or not (chosen = 1, not chosen = 0)
-    rew_present = choose(prob = rew_probabilities[choose_R])
-    #print("Did our choice result in reward? {}".format(choose_R))
+    probabilities = softmax(current_values = values, temperature = gamma)
+    #print("The chance of choosing each option is {}".format(probabilities))
+    choice = choose_option(prob = probabilities)
+    #print("Which option was chosen? {}".format(choice))
+        # left = 0, right = 1 
+    rew_present = get_reward(prob = rew_probabilities[choice])
+    #print("Did our choice result in reward? {}".format(rew_present))
         # rew contains whether reward was obtained or not (obtained = 1, not obtained = 0)
         # if choose_R = 1, then take element 1 in the rew_probabilities (belongs to R resp)
         # if choose_R = 0, then take element 1 in the rew_probabilities (belongs to L resp)
     rew_this_trial = rew_present * rew_per_trial
-    PE, updated_value = rescorla_wagner(previous_value = values[choose_R], 
+    PE, updated_value = rescorla_wagner(previous_value = values[choice], 
                                         obtained_rew = rew_this_trial, learning_rate = alpha)
     # print(PE, updated_value)
-    values[choose_R] = updated_value
+    values[choice] = updated_value
     #print(values)
 print(values)
     
