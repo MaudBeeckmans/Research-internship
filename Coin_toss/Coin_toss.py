ff9f85ebdfe9299ae2c3406e83be8c3e2972b48e
MaudBeeckmans <73498415+MaudBeeckmans@users.noreply.github.com>
2021-10-06 15:37:00 +0200

    Update Coin_toss.py
    * only 2 options now: 
       - HPC = True / False: defines (1) how many different n_trials are used to generate the power plot and (2) which directories to use 
       - fix_simulations = True / False: defines whether simulations are imported (when True) or simulations are created (when False)
         --> either the letters should match the csv-files containing the simulated data or letters define how the simulated dataset will be stored
    * made a separate script for plotting the power afterwards

--- Coin_toss/Coin_toss.py
+++ Coin_toss/Coin_toss.py
@@ -10,47 +10,57 @@ from Coin_toss_estimation import estimate_p_tails
 import numpy as np
 from scipy.stats import pearsonr
 import os 
-
-HPC = False
-import_power = True 
-
-mean_group1 = 0.4
-mean_group2 = 0.7
-general_std = 0.2
-n_pp = 20
-
-if import_power == False or HPC == True: 
-    p_values_group1, data_group1 = simulate_tosses(Mean_prob = mean_group1, Std_prob = general_std, 
-                                                   N_trials = 10000, N_pp = int(n_pp/2))
-    p_values_group2, data_group2 = simulate_tosses(Mean_prob = mean_group2, Std_prob = general_std, 
-                                                   N_trials = 10000, N_pp = int(n_pp/2))
-    data_all_pp = np.row_stack([data_group1, data_group2])
-    all_p_values = np.concatenate([p_values_group1, p_values_group2])
-
-"""When generating 10 000 trials, the mean is not exactly equal to the proposed mean, 
-this is probably due to the cut-off at 0 and 1, thus a bit skewed normal distribution"""
-
-
-
-
-#%%
-
+import pandas as pd
 import scipy.stats as stat
 import pandas as pd 
 
-n_reps = 10000
-# all_used_trials = [5, 10, 100, 500, 1000]
-if HPC == True or import_power == True: 
-    all_used_trials = np.arange(5, 105, 5)
-else: 
-    all_used_trials = [5]
-cut_off_A = 0.9       #formula: cor(p, p_estimates) > 0.8 with n_pp participants
-cut_off_B = 0.05
+HPC = True
+fix_simulations = True
 
-power_A = 'Power_corr' + str(cut_off_A)
-power_B = 'Power_signif_diff'
+letters = ['A', 'B', 'C', 'D']
+letters = ['Z']
+for letter in letters: 
+    imported_data = 'simulated_data' + letter + '.csv'
+    imported_p = 'p_simulations' + letter + '.csv'
+    mean_group1 = 0.4
+    mean_group2 = 0.7
+    general_std = 0.2
+    n_pp = 80
+    if fix_simulations == False: 
+        if fix_simulations == False: 
+            p_values_group1, data_group1 = simulate_tosses(Mean_prob = mean_group1, Std_prob = general_std, 
+                                                           N_trials = 10000, N_pp = int(n_pp/2))
+            p_values_group2, data_group2 = simulate_tosses(Mean_prob = mean_group2, Std_prob = general_std, 
+                                                           N_trials = 10000, N_pp = int(n_pp/2))
+            data_all_pp = np.row_stack([data_group1, data_group2])
+            all_p_values = np.concatenate([p_values_group1, p_values_group2])
+            # pd.DataFrame(data_all_pp).to_csv("simulated_data" + letter + ".csv", header = None, index = None)
+            # pd.DataFrame(all_p_values).to_csv("p_simulations" + letter + ".csv", header = None, index = None)
+    else: 
+        print('Simulations imported')
+        data_all_pp = pd.read_csv(imported_data, header = None)
+        data_all_pp = data_all_pp.values
+        all_p_values = pd.read_csv(imported_p, header = None)
+        all_p_values = all_p_values.values
+        all_p_values = all_p_values.reshape(n_pp)
+    
+    """When generating 10 000 trials, the mean is not exactly equal to the proposed mean, 
+    this is probably due to the cut-off at 0 and 1, thus a bit skewed normal distribution"""
+    
+    #Power estimation for this dataset (1 dataset = n_pp and 10 000 trials per participant)
+    n_reps = 10000
+    # all_used_trials = [5, 10, 100, 500, 1000]
+    if HPC == True: 
+        all_used_trials = np.arange(5, 105, 5)
+    else: 
+        all_used_trials = [5]
+    cut_off_A = 0.9       #formula: cor(p, p_estimates) > 0.8 with n_pp participants
+    cut_off_B = 0.05
+    
+    power_A = 'Power_corr' + str(cut_off_A)
+    power_B = 'Power_signif_diff'
+    
 
-if import_power == False: 
     power_dataframe = pd.DataFrame(columns = ["n_trials", power_A, power_B])
     for current_ntrials in all_used_trials: 
         correlations = np.array([])
@@ -75,52 +85,14 @@ if import_power == False:
             break 
     print(all_p_values)
 
-if HPC == True: 
-    data_dir = os.environ.get('VSC_DATA')
-    target_dir = data_dir + '/Coin_toss'
-    if not os.path.isdir(target_dir): 
-        os.mkdir(target_dir)
-else: 
-    target_dir = os.getcwd()
-
-if import_power == True: 
-    power_dataframe = pd.read_csv("Coin_tossPower_df_corr0.9_20ppB.csv")
-else: 
-    filename = target_dir + 'Power_df_corr0.9_20pp.csv'
+    if HPC == True: 
+        data_dir = os.environ.get('VSC_DATA')
+        target_dir = data_dir + '/Coin_toss'
+        if not os.path.isdir(target_dir): 
+            os.mkdir(target_dir)
+    else: 
+        target_dir = os.getcwd()
+    
+    filename = target_dir + '/Power_df_20pp' + letter + '.csv'
     power_dataframe.to_csv(filename)
-    
-    
-
-#%%
-
-"""When running script on the HPC this part should be commented!"""
-if HPC != True: 
-    from matplotlib import pyplot as plt 
-    power_options = [power_A, power_B]
-    fig, axes = plt.subplots(nrows = 2, ncols = 1)
-    for i in range(2): 
-        axes[i].plot(power_dataframe['n_trials'], power_dataframe[power_options[i]], "ro")
-        axes[i].set_ylim([0, 1.1])
-        axes[i].set_xlim([0, max(all_used_trials) + 10])
-        axes[i].set_xlabel('N_trials', loc = 'right')
-        y_label = axes[i].set_ylabel('P', loc = 'top')
-        y_label.set_rotation(0)
-    axes[0].set_title("{} pp, power = P(corr(p_est, p) > {})".format(n_pp, cut_off_A))
-    axes[1].set_title("{} pp, power = P(pgroup1 < pgroup2) p-value <= {}".format(n_pp, cut_off_B))
-    
-    fig.savefig('Power.png')
-
-
-
-"""
-
-* met 50 pp: correlatie al vanaf 10 trials altijd boven de 80%, dus power = 100% 
-* met 20 pp: power bij 10 trials is 93,18%; 99,13%; 98,8% voor cut-off = 0.8
-* met 10 pp: power bij 10 trials is - 94,54% voor cut-off = 0.8
-                                    - 57.32%, 57.11% voor cut-off = 0.9
-
-
-Remark: als er teveel p = 0 of p = 1 zijn, dan zal dit de correlatie omhoog trekken, want dan 
-is de p_estimate sowieso correct 
-"""
-
+        
