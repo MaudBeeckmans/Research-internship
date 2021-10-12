daa1d673c2c104a504cdc6c813f2854f5b399ff2
MaudBeeckmans <73498415+MaudBeeckmans@users.noreply.github.com>
2021-10-12 11:00:50 +0200

    Update Estimate_likelihood.py
    * changed the elements within function in order to be able to use the function with gradient descent (optimize function)

--- RW_simple/Estimate_likelihood.py
+++ RW_simple/Estimate_likelihood.py
@@ -7,15 +7,20 @@ Created on Fri Oct  8 14:06:08 2021
 
 import numpy as np
 from General_RW_functions import softmax, choose_bandit, get_reward, rescorla_wagner
+import pandas as pd 
 
-def likelihood(learning_rate_estimate = None, temperature_estimate = None, actual_choices = None, 
-               actual_rewards = None, start_values = None):
+def likelihood(learning_rate_estimate, file_name, n_trials):
     """Function to estimate the likelihood of a given parameter combination (learning_rate
     and temperature) given the data. Actual_choices, actual_rewards and start_values are
     drawn from the actual data of the participant. Returns the logL for these parameter values, 
     the higher the logL, the better! Thus if want to minimize, take -logL and find minimum!"""
-    n_trials = actual_choices.shape[0]
-    values = start_values
+    data = pd.read_csv(file_name)
+    real_alpha = data['learning_rate'][0]
+    # n_trials = data.shape[0]
+    values = np.array([0.5, 0.5])
+    actual_rewards = data['rewards'][:n_trials]
+    actual_choices = data['choices'][:n_trials]
+    temperature_estimate = data['temperature'][0]
     logL = 0
     for trial in range(n_trials): 
         #The choice that was made at this trial and whether reward was received
@@ -31,5 +36,5 @@ def likelihood(learning_rate_estimate = None, temperature_estimate = None, actua
                                                 obtained_rew = rew_this_trial, 
                                                 alpha = learning_rate_estimate) 
         values[choice] = updated_value
-    return logL
+    return -logL
         
