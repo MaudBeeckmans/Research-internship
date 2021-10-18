398dc66be1ddbc01bd79c938621c3a0191af6e5a
MaudBeeckmans <73498415+MaudBeeckmans@users.noreply.github.com>
2021-10-18 22:09:48 +0200

    Update CSV_LRandData_creation.py
    Included the option to create a csv-file containing the whole range of learning_rates (0.1-1) instead of a specific group learning_rate.

--- RW_EEG/CSV_LRandData_creation.py
+++ RW_EEG/CSV_LRandData_creation.py
@@ -8,15 +8,26 @@ Created on Wed Oct 13 17:22:00 2021
 from Functions_EEG_experiment import generate_parameters
 import numpy as np 
 
-N_pp = 80
-group_mean = 0.8
-group_std = 0.1
-Output_file = 'pp_overview{}.csv'.format(N_pp)
+all_possible_LR =False
 
-learning_rates = generate_parameters(mean = group_mean, std = group_std, n_pp = N_pp)
+if all_possible_LR == False: 
+    N_pp = 80
+    group_mean = 0.8
+    group_std = 0.1
+    Output_file = 'pp_overview{}_group1.csv'.format(N_pp)
+
+    learning_rates = generate_parameters(mean = group_mean, std = group_std, n_pp = N_pp)
+    pp_numbers = np.arange(0, N_pp, 1)
+    size_array = N_pp
+else: 
+    learning_rates = np.arange(0, 1.1, 0.1)
+    size_array = learning_rates.shape[0]
+    pp_numbers = np.arange(0, size_array, 1)
+    Output_file = 'allLR_overview.csv'
+
 learning_rates = np.round(learning_rates, 3)
-pp_numbers = np.arange(0, N_pp, 1)
-data_selection = np.random.randint(0, 10, size = N_pp)
+data_selection = np.random.randint(0, 10, size = size_array)
+
 
 CSV = np.column_stack([learning_rates, data_selection, pp_numbers])
 np.savetxt(Output_file, CSV, delimiter = ',', fmt = ("%.3f", "%.i", "%.i"),
