61abaf8b2397b89d3a12c415f29ecb0f7272084f
MaudBeeckmans <73498415+MaudBeeckmans@users.noreply.github.com>
2021-10-22 13:11:12 +0200

    Update Parameter_estimation.py
    Changed how we sample the simulations. 
    * each simulation is now stores as 1 responses file; this response file contains the responses for 1 'simulation' over 1000 repetitions
    * the design remains the same for one participant, thus to estimate the parameters 
       - we take the design for this participant and use this same design for all repetitions
       - we take the responses tied to this repetition 
       - we combine the design and the responses for this repetition to estimate the parameter (LR now only) at this repetition
    * Estimation_file lookas the same as before: one estimation file per simulation

--- RW_EEG/Parameter_estimation.py
+++ RW_EEG/Parameter_estimation.py
@@ -14,53 +14,58 @@ import pandas as pd
 
 group = 1 #when using allLR, put group to 'allLR'
 trials =  np.array([50, 100, 200, 300, 480, 480*2, 480*3]) #The number of trials that will be used to estimate the parameter(s)
-used_reps = 1000 # The number of repetitions per participant that will be used to estimate the parameter(s)
+used_reps = 100 # The number of repetitions per participant that will be used to estimate the parameter(s)
+used_temperature = 0.41
+
+if group == 'allLR': N_simulations = 11
+else: N_simulations = 80
 
 
-if group == 'allLR': 
-    N_simulations = 11
-else: 
-    N_simulations = 80
-
-# Define the name of the folder that contains the necessary input variables
-simul_folder = 'Simulations_group{}'.format(group)
-estim_folder = 'Estimations_group{}'.format(group)
-
-
+# Define for which simulation we'll currently do the estimations at each repetition with each possible number of trials in trials used for the estimation
 simul_number = sys.argv[1:] #Get the params from another file (in CSV file)
 print(simul_number)
 assert len(simul_number) == 1     #Checks whether there are n params
-simul = simul_number[0] 
+simul = int(simul_number[0])
 
 
+# Define the name of the folder that contains the necessary input variables
+#Which_design: from the overview file, see which design_file was used to simulate the data of this simulation
+    #.iloc[a, b]: a = the row corresponding to this simulation number, b = the column containing the number of the design file used for this simulation
+which_design = pd.read_csv('pp_overview{}_group{}.csv'.format(N_simulations, group)).iloc[simul, 1] 
+start_design = pd.read_csv(os.path.join(os.getcwd(), 'Design', 'Data{}.csv'.format(which_design)))
+# the trials within the design were repeated three times when simulating the responses (shape = 1440 x 10)
+start_design = start_design.append(start_design).append(start_design)
+#Get the correct responses from the responses_file (shape = 1000x1440)
+responses_allreps = pd.read_csv(os.path.join(r'/kyukon/scratch/gent/442/vsc44254/RW_EEG/Responses_group{}'.format(group), 
+                                             'Responses_simulation{}.csv'.format(simul)), header = None)
+responses_allreps = np.array(responses_allreps)
 
+# Define the name of the folder that will contain the estimations for each simulation within this group
+estim_folder = 'Estimations_group{}'.format(group) # Folder name
+Estimation_folder = os.path.join(r'/kyukon/scratch/gent/442/vsc44254/RW_EEG/', estim_folder) # Complete path 
 
-
-
-
-folder_simul_name = os.path.join('/kyukon/scratch/gent/442/vsc44254/RW_EEG/', simul_folder, 
-                                 'Simulation{}'.format(simul))
-Estimation_folder = os.path.join(r'/kyukon/scratch/gent/442/vsc44254/RW_EEG/', estim_folder)
-
+# Create the folder that will contain the estimations for this group (1 file per simulation)
 if not os.path.isdir(Estimation_folder):
     os.makedirs(Estimation_folder)
 
 
-# simul_files = os.listdir(folder_simul_name)#Not used anymore since this doesn't give the correctorder!
+# Create the file (with path) to which the estimation_DF will be written eventually 
 Estimation_file = os.path.join(Estimation_folder, 'Estimate_pp{}.csv'.format(simul))
+# Create the dataframe that will contain all the estimations for this simulation (shape will be: n_reps x n_cols)
 estimation_DF = pd.DataFrame(columns = np.concatenate([['real_param'], trials.astype(str), ['rep']]))
 
-file_count = 0 #keep track of the file number we're currently at
-simul_files = []
-for i in range(used_reps): simul_files = np.append(simul_files, "Simulation{}rep{}.csv".format(simul, i))
-# print(simul_files)
-for simul_file in simul_files: 
-    file_name = os.path.join(folder_simul_name, simul_file)
-    estimation_DF = estimation_DF.append({'rep': file_count}, ignore_index = True)
+# Do the estimation over all repetitions for this simulation 
+for this_rep in range(used_reps): 
+    # Combine the start_design of this simulation and the responses given within this repetition
+    start_design['Response'] = responses_allreps[this_rep, :]
+    # Create some space to fill in the estimation_DF 
+    estimation_DF = estimation_DF.append({'rep': this_rep}, ignore_index = True)
     for n_trials in trials: #trials: contains how many trials will be used to estimate the parameter(s)
-        # print('\n {}'.format(n_trials))
-        estim_param = optimize.fminbound(likelihood, 0, 1, args =(tuple([file_name, n_trials])), 
+        # estimate the parameters given the start_design; n_trials defines how many trials are used to estimate the parameter(s) 
+        estim_param = optimize.fminbound(likelihood, 0, 1, args =(tuple([start_design, n_trials, used_temperature])), 
                             maxfun= 100000, xtol = 0.001)
-        estimation_DF[str(n_trials)][file_count] = estim_param
-    file_count += 1
+        # store the best fitting parameter(s) in the estimation_DF in the correct column (n_trials) and correct row (this_rep)
+        estimation_DF[str(n_trials)][this_rep] = estim_param
+
+# Save the estimations 
 estimation_DF.to_csv(Estimation_file)
