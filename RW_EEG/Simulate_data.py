c6a2e8c09ba928f978292ba141d292da832f4c77
MaudBeeckmans <73498415+MaudBeeckmans@users.noreply.github.com>
2021-10-25 15:02:15 +0200

    Update Simulate_data.py
    Deleted some irrelevant lines of code. And now adaptation to 4 inputs from the csv-file created with CSV_LRandData_creation.py (LR, Temp, Data, pp_number).

--- RW_EEG/Simulate_data.py
+++ RW_EEG/Simulate_data.py
@@ -11,34 +11,36 @@ import sys, os
 import numpy as np 
 
 
-nreps = 1000            #for each pp. generate 1000 'experiment-executions' 
-group = 3 #when using allLR, put group to 'allLR'
+nreps = 100          #for each pp. generate 1000 'experiment-executions' 
+group = 1 #when using allLR, put group to 'allLR'
+temp_type = 'variable' # should be variable or fixed
 
 if group == 'allLR': 
     output_folder = 'Simulations_all_LR'
-    csv_file = 'allLR_overview.csv'
 
 else: 
-    output_folder = 'Simulations_group{}'.format(group)
-    csv_file = 'pp_overview80_group{}.csv'.format(group)
+    output_folder = 'Simulations_group{}_Temp{}'.format(group, temp_type)
 
 
 params = sys.argv[1:]   #Get the params from another file (in CSV file)
-assert len(params) == 3     #Checks whether there are 3 params
+assert len(params) == 4     #Checks whether there are 3 params
 # assign to some parameters
-LR, design_filename, pp_number = sys.argv[1:]
+LR, Temp, design_filename, pp_number = sys.argv[1:]
 LR = np.float(LR)
+Temp = np.float(Temp)
 design_filename = int(design_filename)
 design_filename = 'Data{}.csv'.format(str(design_filename))
 pp_number = int(pp_number)
-Output_folder = os.path.join(os.environ.get('VSC_SCRATCH'), 'RW_EEG', output_folder, 
-                             'Simulation{}'.format(str(pp_number)))
+Output_folder = os.path.join(os.environ.get('VSC_DATA'), 'RW_EEG', output_folder)
 if not os.path.isdir(Output_folder): 
         os.makedirs(Output_folder)
 Design_file = os.path.join(os.environ.get('VSC_HOME'), 'RW_EEG', 'Design', design_filename)
 
 for rep in range(nreps): 
-    Output_file = os.path.join(Output_folder, 'Simulation{}rep{}.csv'.format(str(pp_number), str(rep)))
-    total_reward = simulate_RW(learning_rate = LR, design_file = Design_file, 
-                               output_file = Output_file, triple_trials = True)
-    print(rep)
+    total_reward, responses = simulate_RW(learning_rate = LR, design_file = Design_file, temperature = Temp, 
+                                          triple_trials = True)
+    if rep == 0: store_responses = responses
+    else: store_responses = np.row_stack([store_responses, responses])
+
+Output_file = os.path.join(Output_folder, 'Simulation{}.csv'.format(str(pp_number)))
+np.savetxt(Output_file, store_responses, fmt = '%.i', delimiter = ',') 
